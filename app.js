ade8c07466f9bd28c481ed1ea6ba64906370bff6
Karl Neumann <karl.neumann@symbiosis.at>
2011-03-27 12:36:31 +0000

    mongodb authentication and use of environment variables

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
 var sys = require("sys"), 
     express = require("express"),
-	ws= require("websocket-server"),
+	ws = require("websocket-server"),
 	mongo = require('mongodb');
 
-var host = 'flame.mongohq.com';
-var port = 27097; //mongo.Connection.DEFAULT_PORT;
+var host = process.env.MONGO_HOST;
+var port = process.env.MONGO_PORT;
+var username = process.env.MONGO_USERNAME;
+var password = process.env.MONGO_PASSWORD;
 
-// mongodb://<user>:<password>@flame.mongohq.com:27097/contacts
-sys.puts("creating db { host: " + host + ", port: " + port);
+sys.puts("creating db \n{\n    host: " + host + ",\n    port: " + port + "\n}");
+
 var db = new mongo.Db('contacts', new mongo.Server(host, port, {}), {});
-db.open(function (err, db) {
-    sys.puts(sys.inspect(db));
-});
 
+sys.log("opening db");
 
-db.createCollection('contact', function (err, collection) {
-});
+db.open(function (err, x) {
+		sys.log("authenticating user");
+		db.authenticate(username, password, function (err, val) {
+				if (err !== null) {
+					sys.log("Error authenticating");
+					// perhaps this case should exit also
+				}
+				sys.log("Authenticated.");
+
+				db.createCollection('contact', function (err, collection) {
+				});
+
+			});
+	});
 
 var app = express.createServer();
 app.use(express.staticProvider(__dirname + "/public"));
@@ -25,8 +37,6 @@ app.get("/", function (req, res) {
 	});
 
 app.listen(3000);
-
-
 
 var server = ws.createServer({
 		server: app
@@ -40,7 +50,7 @@ server.addListener("connection", function (conn) {
 		sys.log("client connecting: " + conn.id + ".");
 		conn.addListener("message", function (message) {
 			sys.log("client sending message: " + conn.id + ".");
-			
+
 			db.collection('contact', function (err, collection) {
 					sys.log(message);
 					collection.insert(JSON.parse(message), function (err, obj) {
@@ -48,9 +58,9 @@ server.addListener("connection", function (conn) {
 								type: "contact",
 								data: obj
 							};
-							
+
 							conn.send(JSON.stringify(returnMessage));
-							
+
 							collection.count(function (err, count) {
 									var returnMessage = {
 										type: "count",
@@ -59,7 +69,7 @@ server.addListener("connection", function (conn) {
 
 									conn.send(JSON.stringify(returnMessage));
 								});
-							
+
 							collection.find(function (err, cursor) {
 									cursor.toArray(function (err, docs) {
 											var returnMessage = {
@@ -78,6 +88,3 @@ server.addListener("connection", function (conn) {
 server.addListener("close", function (conn) {
 		sys.log("client closing connection: " + conn.id + ".");
 	});
-
-
-
